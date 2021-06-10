ff3256967d94f0f707f0a367797bbee388532784
Florian Beutler <florian.beutler@port.ac.uk>
2021-06-10 09:58:40 +0100

    Update create_Wll.py

--- create_Wll.py
+++ create_Wll.py
@@ -18,7 +18,7 @@ def create_W(kbins, s_win, window, outpath=''):
 	OUTPUT 
 	Window function W_{ell,ell'}(k,k') as given in eq. 2.5 of arxiv:XXX
 	-> This window function needs to be averaged to the observational
-	and theoretical bins as shown in eq. 2.16 (also see appendix E)
+	and theoretical k-bins as shown in eq. 2.16 (also see appendix E)
 	'''
 	smin = min(s_win)
 	smax = max(s_win)
@@ -57,7 +57,7 @@ def create_W(kbins, s_win, window, outpath=''):
 					IntegrandPi = np.zeros(Nfft)
 					for L in range(0, 5): # L = 0,1,2,3,4 : Q_L
 
-						interp_QL = interp1d(s_win, window[n][L], kind='linear', fill_value=(1. if L == 0 else 0.,0.), bounds_error=False)
+						interp_QL = interp1d(s_win, window[n][L], kind='linear', fill_value=((1. if L == 0 else 0.),0.), bounds_error=False)
 
 						if n == 0 or n == 2:
 							IntegrandPi += C0_n02[ell][ellp][L]*interp_QL(slog)*sp.spherical_jn(ell,slog*k) 
@@ -65,37 +65,32 @@ def create_W(kbins, s_win, window, outpath=''):
 							IntegrandPi += C0_n1[ell][ellp][L]*interp_QL(slog)*sp.spherical_jn(ell,slog*k) 
 					kp, Pi = xi2P(slog, IntegrandPi, l=ellp, n=0)
 
-					if ell%2 == 1:
-						prefactor = -1.
-					else:
-						prefactor = 1.
+					prefactor = (-1. if ell%2 == 1 else 1.)
 					prefactor *= (-1. if ell == 1 or ell == 2 else 1.)
-
-					if ellp%2 == 0:
-						prefactor *= 1.
-					else:
-						prefactor *= -1.
+					prefactor *= (1. if ellp%2 == 0 else -1.)
 
 					W[ik] = prefactor*(2./np.pi)*(Pi.real if ellp%2 == 0 else Pi.imag)/(4.*np.pi)
 
-				# Write to file W_{ell,ell'} /////////////////////////
-				filename = "%s/W%d%d%d_%d_%d.dat" % (outpath, n, ell, ellp, Nfft, Nk)
-				with open(filename, 'w') as f:
-					for ikp in range(0, Nfft):
-						for ik in range(0, Nk):
-							f.write("%0.16f " % W[ik][ikp])
-						f.write("\n")
+				if outpath:
+					# Write to file W_{ell,ell'} /////////////////////////
+					filename = "%s/W%d%d%d_%d_%d.dat" % (outpath, n, ell, ellp, Nfft, Nk)
+					with open(filename, 'w') as f:
+						for ikp in range(0, Nfft):
+							for ik in range(0, Nk):
+								f.write("%0.16f " % W[ik][ikp])
+							f.write("\n")
 
-	filename = "%s/kp_%d_%d.dat" % (outpath, Nfft, Nk)
-	with open(filename, 'w') as f:
-		for ikp in range(0, Nfft):
-			f.write("%0.16f\n" % kp[ikp])
+	if outpath:
+		filename = "%s/kp_%d_%d.dat" % (outpath, Nfft, Nk)
+		with open(filename, 'w') as f:
+			for ikp in range(0, Nfft):
+				f.write("%0.16f\n" % kp[ikp])
 
-	filename = "%s/k_%d_%d.dat" % (outpath, Nfft, Nk)
-	with open(filename, 'w') as f:
-		for ik in range(0, Nk):
-			if isinstance(kbins[ik], float):
-				f.write("%0.16f\n" % kbins[ik])
-			else:
-				f.write("%0.16f\n" % 0.)
+		filename = "%s/k_%d_%d.dat" % (outpath, Nfft, Nk)
+		with open(filename, 'w') as f:
+			for ik in range(0, Nk):
+				if isinstance(kbins[ik], float):
+					f.write("%0.16f\n" % kbins[ik])
+				else:
+					f.write("%0.16f\n" % 0.)
 	return W
